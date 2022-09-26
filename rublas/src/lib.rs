a0fbb21f8cebaaea4761d0c0dbe4bced45fcb057
Albert Shi <codefisheng@gmail.com>
2022-09-26 22:39:37 +0800

    setup features compile option for backend: openblas + netlib

--- rublas/src/lib.rs
+++ rublas/src/lib.rs
@@ -1,16 +1,12 @@
-
 #[cfg(test)]
 mod tests {
     use ndarray::arr2;
 
     #[test]
     fn it_works() {
-        let a = arr2(&[[1, 2, 3],
-                       [4, 5, 6]]);
+        let a = arr2(&[[1, 2, 3], [4, 5, 6]]);
 
-        let b = arr2(&[[6, 3],
-                       [5, 2],
-                       [4, 1]]);
+        let b = arr2(&[[6, 3], [5, 2], [4, 1]]);
 
         println!("{}", a.dot(&b));
 
