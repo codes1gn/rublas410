a77a23d2e617d7123b6719d1d2b46b35a7d9f73d
Zoey Song <zoey.song@enflame-tech.com>
2022-10-21 14:06:41 +0800

    Add a benchmark test for sub_f32

--- rublas/benches/sub.rs
+++ rublas/benches/sub.rs
@@ -15,13 +15,13 @@ use criterion::*;
 use rublas::prelude::*;
 
 
-fn add_f32(crit: &mut Criterion) {
-    let mut bench_group = crit.benchmark_group("addf32");
+fn sub_f32(crit: &mut Criterion) {
+    let mut bench_group = crit.benchmark_group("subf32");
     bench_group.sample_size(10);
     
     for Msize in vec![16, 256, 1024, 4096].iter() {
         for Ksize in vec![16, 256, 1024, 4096].iter() {
-            //addf32_ndarray
+            //subf32_ndarray
             bench_group.bench_with_input(
                 BenchmarkId::new(format!("M{}_K{}_ndarray_f32", Msize, Ksize), 0),
                 Msize,
@@ -29,12 +29,12 @@ fn add_f32(crit: &mut Criterion) {
                     let lhs = Array2::<f32>::ones((*Msize, *Ksize));
                     let rhs = Array2::<f32>::ones((*Msize, *Ksize));
                     bench.iter(|| {
-                        black_box(&lhs + &rhs);
+                        black_box(&lhs - &rhs);
                     });
                 },
             );
             
-            //addf32_owned
+            //subf32_owned
             bench_group.bench_with_input(
                 BenchmarkId::new(format!("M{}_K{}_owned_f32", Msize, Ksize), 0),
                 Msize,
@@ -43,12 +43,12 @@ fn add_f32(crit: &mut Criterion) {
                     bench.iter(|| {
                         let lhs = BlasTensor::ones(vec![*Msize, *Ksize]);
                         let rhs = BlasTensor::ones(vec![*Msize, *Ksize]);
-                        black_box(exec.addf32_owned(lhs, rhs));
+                        black_box(exec.subf32_owned(lhs, rhs));
                     });
                 },
             );
 
-            //addf32_side_effect
+            //subf32_side_effect
             bench_group.bench_with_input(
                 BenchmarkId::new(format!("M{}_K{}_side_effect_f32", Msize, Ksize), 0),
                 Msize,
@@ -58,7 +58,7 @@ fn add_f32(crit: &mut Criterion) {
                     let mut out = BlasTensor::zeros(vec![*Msize, *Ksize]);
                     let exec = BlasExecutor::new();
                     bench.iter(|| {
-                        black_box(exec.addf32_side_effect(&lhs, &rhs, &mut out));
+                        black_box(exec.subf32_side_effect(&lhs, &rhs, &mut out));
                     });
                 },
             );
@@ -66,5 +66,5 @@ fn add_f32(crit: &mut Criterion) {
     }
 }
 
-criterion_group!(add_tests, add_f32);
-criterion_main!(add_tests);
+criterion_group!(sub_tests, sub_f32);
+criterion_main!(sub_tests);
